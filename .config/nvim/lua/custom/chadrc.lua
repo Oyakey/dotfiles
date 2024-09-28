ae06918308c2912ded6a00ed4ccee769381649c8
Julien Lecarpentier <julien@latelier-conceptionweb.com>
2024-09-28 10:13:00 +0200

    feat: new vim config + oh my zsh

--- .config/nvim/lua/custom/chadrc.lua
+++
@@ -1,6 +0,0 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'catppuccin'}
- M.plugins = 'custom.plugins'
- -- M.mappings = 'custom.mappings'
- return M
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,26 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "prettier",
+        "prettierd",
+        "typescript-language-server",
+      },
+    },
+  },
+}
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
+})
+
+return plugins
